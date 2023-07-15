457ff47320cea9f902957cd9a9a272763d20e2c3
Miguel Arroyo Calvo <miguelarroyocalvo@MacBook-Pro-de-Miguel.local>
2023-07-15 02:39:23 +0200

    Media clase

--- mi_cartera_js/statics/js/app.js
+++ mi_cartera_js/statics/js/app.js
@@ -1,10 +1,31 @@
+var el_ultimo
+
 function pulsado(event){
     event.preventDefault()
-    alert("Has hecho click")
+    let the_father = document.querySelector("#contenido")
+    let the_paragraph = document.createElement("p")
+
+    let input = document.querySelector("#texto")
+
+    the_paragraph.innerHTML = input.value
+    the_father.appendChild(the_paragraph)
+
+    el_ultimo = the_paragraph
+
+}
+
+function borrado(event){
+    event.preventDefault()
+
+    let the_father = document.querySelector("#contenido")
+    the_father.removeChild(el_ultimo)
+
 }
 
 window.onload = function(){
     let botton = document.querySelector("#btnSubmit")
-
     boton.addEventListener("click", pulsado)
+
+    let botonBorra = document.querySelector("#btnDelete")
+    botonBorra.addEventListener('click', borrado)
 }
